2adc887b5f6af5e599b6b508be657de7e5e5fb09
Ignat Georgiev <ignat.georgiev1@gmail.com>
2015-09-25 19:50:42 +0300

    Stack not printing stuff

--- Stack.cpp
+++ Stack.cpp
@@ -7,12 +7,13 @@ using namespace std;
 class Stack{
 	int data_[STACK_SIZE];
 	int top_;
+	
 public:
 	Stack(){
 		top_ = 0;
 	}
 	bool is_full(){
-		return top_ == 0;
+		return top_ = 0;
 	}
 	bool is_empty(){
 		return top_ = STACK_SIZE;
@@ -38,12 +39,13 @@ public:
 };
 
 int main(){
+
 	Stack st;
-	for(int i = 0; i < 10; i++){
+	for(int i = 0; i <= 10; i++){
 		st.push(i);
 	}
 
-	for(int i = 0; i < 10; i++){
+	for(int i; i > 0; i--){
 		cout << st.pop() << endl;
 	}
 }
